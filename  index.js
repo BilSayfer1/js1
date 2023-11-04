f49fef5e8bd5b7adb84289c82a4147c5e9bd45df
BilSayfer1 <alsafirbozorov5@gmail.com>
2023-11-04 07:50:10 +0500

    first commit

---
+++  index.js
@@ -0,0 +1,18 @@
+let name = "Джон"
+let admin = name
+
+console.log(admin)
+
+
+let balance = 25000
+
+let car = 4480
+let food = 890
+
+let invoice = 5500
+let stock = 4200
+
+let totalBalance = 0
+console.log(balance - car - food + invoice + stock)
+
+
